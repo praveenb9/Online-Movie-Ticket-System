c7072ece959d8f7e711789f9c7f7af0690cebccd
radha krishna <radha krishna@jyo-sri.mshome.net>
2020-08-12 15:59:45 +0530

    Movie ms All model classes created...

---
+++ omts-movie-ms/src/main/java/com/capg/omts/movie/OmtsMovieMsApplication.java
@@ -0,0 +1,13 @@
+package com.capg.omts.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmtsMovieMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmtsMovieMsApplication.class, args);
+	}
+
+}
---
+++ omts-movie-ms/src/test/java/com/capg/omts/movie/OmtsMovieMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.omts.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmtsMovieMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
