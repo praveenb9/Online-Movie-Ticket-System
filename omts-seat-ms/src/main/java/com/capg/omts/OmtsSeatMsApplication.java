47f686ec29580985f1e890c20012e2f9bf7af49d
santoshkalluri <santoshkalluri@users.noreply.github.com>
2020-08-21 20:24:50 +0530

    Seat

--- omts-seat-ms/src/main/java/com/capg/omts/OmtsSeatMsApplication.java
+++ omts-seat-ms/src/main/java/com/capg/omts/OmtsSeatMsApplication.java
@@ -2,8 +2,11 @@ package com.capg.omts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class OmtsSeatMsApplication {
 
 	public static void main(String[] args) {
