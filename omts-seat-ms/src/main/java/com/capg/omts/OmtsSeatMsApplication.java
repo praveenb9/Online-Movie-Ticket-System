f02059f51420e01d8216bec5f545f1fc52577121
santoshkalluri <santoshkalluri@users.noreply.github.com>
2020-08-23 19:04:51 +0530

    jenkins and sonar

--- omts-seat-ms/src/main/java/com/capg/omts/OmtsSeatMsApplication.java
+++ omts-seat-ms/src/main/java/com/capg/omts/OmtsSeatMsApplication.java
@@ -2,10 +2,14 @@ package com.capg.omts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 
 public class OmtsSeatMsApplication {
 
